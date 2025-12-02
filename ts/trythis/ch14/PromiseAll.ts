1d48b28283f83587508c5b62e0580a709879161d
KelseyKwon <nykwon7777@gmail.com>
2025-12-02 16:28:59 +0900

    feat : 연습문제 - promiseAll : convert type JS to TS 정답

--- ts/trythis/ch14/PromiseAll.ts
+++ ts/trythis/ch14/PromiseAll.ts
@@ -1,5 +1,4 @@
-const assert = require("assert");
-
+import assert from "assert";
 // sec초를 최대값으로 하는 랜덤 시간 후에 sec를 resolve!
 // const randTime = (sec) =>
 //   new Promise((resolve) => {
@@ -15,6 +14,7 @@ const randTime = (sec: number): Promise<number> =>
 type settledResult<T> =
   | { status: "fulfilled"; value: T }
   | { status: "rejected"; reason: any };
+
 type PromiseObject<T> = Promise<T>[];
 
 /**
@@ -50,6 +50,9 @@ Promise.allSettled([randTime(1), Promise.reject("RRR"), randTime(3)]).then(
     promiseAllSettled([randTime(1), Promise.reject("RRR"), randTime(3)])
       .then((array) => {
         console.table(array);
+        const x = array[0];
+        if (x?.status === "fulfilled") console.log(x.value);
+        else console.log("dd");
         console.log("여긴 과연 호출될까?111!");
         assert.deepStrictEqual(array, orgArr);
       })
@@ -59,6 +62,7 @@ Promise.allSettled([randTime(1), Promise.reject("RRR"), randTime(3)]).then(
   }
 );
 
+// returntype은 그냥 함수가 반환하는 타입에 Promise를 감싸면 된다.
 function promiseAll<T>(parr: PromiseObject<T>): Promise<T[]> {
   return new Promise((resolve, reject) => {
     const results: T[] = [];
@@ -97,3 +101,61 @@ Promise.all([randTime(2), Promise.reject("RRR"), randTime(2.5)])
     console.log("orgCatch>>", err);
     assert.strictEqual(err, "RRR");
   });
+
+// new Promise((resolve) => randTime().then(resolve))
+/**
+ *
+ * @returns 1초 후에 r1을 얻고 출력한다.
+ */
+async function f() {
+  const r1 = await randTime(1);
+  console.log("🚀 ~ r1:", r1);
+  return r1;
+}
+
+/**
+ *
+ * @returns new Promise를 만들어서 1초 후에 resolve로 넘겨준다.
+ */
+function f2() {
+  return new Promise((resolve) =>
+    randTime(1).then((r2) => {
+      console.log("🚀 ~ r2:", r2);
+      resolve(r2);
+    })
+  );
+}
+f();
+f2();
+
+const myFetch = async (url: string) => {
+  const res = await fetch(url);
+  const rrr = await res.json();
+  console.log("🚀 ~ rrr:", rrr);
+  return rrr;
+};
+
+const myFetch2 = async (url: string) => fetch(url).then((res) => res.json());
+
+// value가 Promise인 iterator이다.
+function iter<T extends number[]>(vals: T) {
+  let i = -1;
+  return {
+    next() {
+      i += 1;
+      return { value: randTime(vals[i]!), done: i >= 3 };
+    },
+  };
+}
+
+(async function () {
+  const it = iter([1, 2, 3]);
+  console.time("iter");
+  const { value } = it.next();
+  console.log("🚀 ~ value:", await value);
+  // console.log('11=', await it.next().value);
+  // console.log('2=', await it.next());
+  // console.log('3=', await it.next());
+  // console.log('4=', await it.next());
+  console.timeEnd("iter");
+})();
