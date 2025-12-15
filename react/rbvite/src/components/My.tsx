20fef36f3842a09e6078ae3177171fe01a2e89f2
KelseyKwon <nykwon7777@gmail.com>
2025-12-15 16:39:52 +0900

    [React] useReducer

--- react/rbvite/src/components/My.tsx
+++ react/rbvite/src/components/My.tsx
@@ -2,7 +2,7 @@ import Profile, { type ProfileHandler } from '../Profile';
 import Login from '../Login';
 import Button from './ui/Button';
 import { PlusIcon } from 'lucide-react';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import Item from './Item';
 import { useSession } from '../hooks/SessionContext';
 
@@ -11,13 +11,28 @@ import { useSession } from '../hooks/SessionContext';
 
 export default function My() {
   const {session } = useSession();
-  const [isAdding, setAdding]= useState(false);
+  // const [isAdding, setAdding]= useState(false);
+  // const toggleAdding = () => setAdding((pre) => !pre);
+
+  // first argument : Dispatch 함수
+  const [isAdding, toggleAdding] = useReducer(pre => !pre, false); //여기에서는 action이 필요가 없다. 
+  // addPrice(1000) -> 이전에 있던 total 값에 더한 것이 -> useReducer()
+  /*
+  function useReducer(reducer, initValueOrFunction) {
+    const [state, setState] = useState(initValueOrFunction);
+    const dispatch = (action) => {
+      // 기존에 갖고있던 값을 받아서 (true) -> false 이런식으로 반환한다. 
+      setState(reducer(preState, action));
+    }
+    return [state, dispatch];
+  }
+    */
+
   const profileHandlerRef = useRef<ProfileHandler>(null);
-  const item101 = session.cart.find(item => item.id === 101);
+
   // useEffect -> dom이 그려지고 나서 호출이 된다!
   useEffect(() => {
-    console.log(item101)
-  }, [item101])
+  }, [])
 
 return (
     <>
@@ -27,7 +42,7 @@ return (
         e.preventDefault();
         profileHandlerRef.current?.showLoginUser();
         profileHandlerRef.current?.logout();
-      }}>{item101?.name}</a>
+      }}>{}</a>
       <ul>
         {/* destructuring! */}
 
@@ -42,10 +57,10 @@ return (
           {isAdding ? (
             <Item
               item={{ id: 0, name: 'New Item', price: 3000 }}
-              toggleAdding={() => setAdding(false)}
+              toggleAdding={toggleAdding}
             />
           ) : (
-            <Button onClick={() => setAdding(true)} className=''>
+            <Button onClick={toggleAdding} className=''>
               <PlusIcon />
             </Button>
           )}
--- react/rbvite/src/hooks/CounterContext.tsx
+++ react/rbvite/src/hooks/CounterContext.tsx
@@ -1,21 +1,42 @@
-import { createContext, use, useState, type PropsWithChildren } from "react";
+// count라는 상태를 보관하는 Context
+import { createContext, use, useReducer, type PropsWithChildren } from "react";
 
 type ContextValue = {
     count: number;
     plusCount: () => void;
+    minusCount: () => void;
+    multiCount: (n:number) => void;
 };
 
 // 1. create Context
 // CounterContext는 undefined을 가질수도 있다 -> 여기서 undefined을 걸 수 있도록 하면, 나중에?을 다는 등 귀찮아 지므로 초기 값을 주자!
-const CounterContext = createContext<ContextValue>({count: 0, plusCount: () => {}});
+const CounterContext = createContext<ContextValue>({count: 0, plusCount: () => {}, minusCount: () => {}, multiCount: () => {}});
 
+type Action = {
+    type: 'plus' | 'minus' | 'multi' 
+    payload: number
+};
+
+// return 은 항상 precount 타입이 된다. 
+const reducer = (preCount: number, {type, payload}: Action) => {
+    switch (type) {
+        case 'plus': return preCount + payload;
+        case 'minus': return preCount - payload;
+        case 'multi': return preCount * payload;
+        default: return preCount;
+    }
+}
 // 2. Provider => component = function!
 // CounterContext.Provider value = {{x:1, y: () => {}}} 이런식으로 쓸려면, x, y를 사용하는 쪽에서 어떻게 써야 하나?
 export function CounterProvider ({children}: PropsWithChildren) {
-const [count, setCount] = useState(0);
-  const plusCount = () => setCount((prevCount) => prevCount + 1);
+    const [count, dispatch] = useReducer(reducer, 0);
+    // 이렇게 계속 늘어나는 것을 useReducer로 관리한다!
+// const [count, setCount] = useState(0);
+  const plusCount = () => dispatch({type: 'plus', payload: 1});
+  const minusCount = () => dispatch({type: 'minus', payload: 1});
+  const multiCount = (payload:number) => dispatch({type: 'multi', payload});
 
-    return <CounterContext.Provider value={{count, plusCount}}>
+    return <CounterContext.Provider value={{count, plusCount, minusCount, multiCount}}>
         {/* children들이 consumer가 된다 -> 위에 value을 사용할 수 있다! */}
         {children} 
     </CounterContext.Provider>
